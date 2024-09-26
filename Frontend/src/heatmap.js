79f11665e3e03a1f1ed38ae3621c02ffc6cacf14
ScheWann <92758590+ScheWann@users.noreply.github.com>
2024-09-25 23:36:09 -0500

    Update heatmap.js

--- Frontend/src/heatmap.js
+++ Frontend/src/heatmap.js
@@ -2,10 +2,6 @@ import React, { useEffect } from 'react';
 import * as d3 from 'd3';
 
 export const Heatmap = () => {
-    const getCellIndex = (i1, j1) => {
-        return `x:${i1}, y:${j1}`;
-    };
-
     useEffect(() => {
         const margin = { top: 50, right: 30, bottom: 50, left: 50 };
         const width = 700 - margin.left - margin.right;
