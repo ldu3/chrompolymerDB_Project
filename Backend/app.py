bbf1dc1525af34ba7d9128068cf99cbaba1bb959
ScheWann <92758590+ScheWann@users.noreply.github.com>
2024-10-17 21:37:37 -0500

    generate the api for getting list of chromosome

--- Backend/app.py
+++ Backend/app.py
@@ -1,27 +1,26 @@
 from flask import Flask, jsonify, request, render_template
-from process import ( chromosomes_list, chromosome_sequences, matched_chromosome_data )
-import os
-import psycopg2
+from process import chromosomes_list, getChromosData
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-# data root path
-data_path = '../data'
+data_path = '../Data'
+@app.route('/')
+def index():
+    return 'Hello, World!'
+
 
 @app.route('/getChromosList', methods=['GET'])
 def get_ChromosList():
-    return jsonify(chromosomes_list(data_path))
+    return jsonify(chromosomes_list())
 
-@app.route('/getChromosSeq', methods=['POST'])
-def get_ChromosSeq():
-    chromosome_name = request.json['chromosome_name']
-    return jsonify(chromosome_sequences(data_path, chromosome_name))
 
-@app.route('/getChromosData', methods=['POST'])
-def get_ChromosData():
-    chromosome_name = request.json['chromosome_name']
-    chromosomeSequence = request.json['chromosomeSequence']
-    return jsonify(matched_chromosome_data(data_path, chromosome_name, chromosomeSequence).to_dict(orient='records'))
+# @app.route('/getChromosData', methods=['POST'])
+# def get_ChromosData():
+#     chromosome_name = request.json['chromosome_name']
+#     chromosomeSequence = request.json['chromosomeSequence']
+#     return jsonify(matched_chromosome_data(data_path, chromosome_name, chromosomeSequence).to_dict(orient='records'))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- Backend/process.py
+++ Backend/process.py
@@ -1,94 +1,95 @@
 import pandas as pd
+import psycopg2
 import os
 import re
 import glob
+from psycopg2.extras import RealDictCursor
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.getenv("DB_HOST")
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+
+def get_db_connection():
+    """Establish a connection to the database."""
+    conn = psycopg2.connect(
+        host=DB_HOST,
+        database=DB_NAME,
+        user=DB_USERNAME,
+        password=DB_PASSWORD,
+        cursor_factory=RealDictCursor,
+    )
+    return conn
 
 
 """
 Returns the list of chromosomes in the given data path
 """
-def chromosomes_list(data_path):
-    chromosomes = set()
-
-    # Iterate through the folders in the given path
-    for folder_name in os.listdir(data_path):
-        full_path = os.path.join(data_path, folder_name)
-        if os.path.isdir(full_path):
-            # Extract the chromosome Name
-            chrom_part = folder_name.split('.')[0]
-            chromosomes.add(chrom_part)
-
-    # Sort chromosomes, placing numeric ones first, followed by 'X'
-    def chromosome_sort_key(chrom):
-        try:
-            # Numeric chromosomes sorted by their value
-            return (int(chrom[3:]), '')  
-        except ValueError:
-            # Non-numeric ones like 'X' are placed last
-            return (float('inf'), chrom)  
-
-    sorted_chromosomes = sorted(chromosomes, key=chromosome_sort_key)
-
-    # Convert to the desired object array format
-    object_array = [{'value': chrom, 'label': chrom} for chrom in sorted_chromosomes]
-
-    return object_array
-
-
-"""
-Returns the list of chromosome sequences in the given data path
-"""
-def chromosome_sequences(data_path, chromosome_name):
-    result = []
-    pattern = re.compile(rf'({chromosome_name})\.(?P<start>\d+)\.(?P<end>\d+)')
+def chromosomes_list():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("""
+        SELECT DISTINCT chrid
+        FROM non_random_hic
+    """)
+    chromosomes = cur.fetchall()
     
-    # List all folders in the directory
-    for folder in os.listdir(data_path):
-        match = pattern.match(folder)
+    def sort_key(chromosome):
+        match = re.match(r"chr(\d+|\D+)", chromosome['chrid'])
         if match:
-            result.append({
-                'start': match.group('start'),
-                'end': match.group('end')
-            })
+            value = match.group(1)
+            return (int(value) if value.isdigit() else float('inf'), value)
+        return (float('inf'), '')
     
-    return result
+    # Sort the chromosomes by the numeric or character suffix
+    sorted_chromosomes_list = sorted(chromosomes, key=sort_key)
+    sorted_chromosomes_list = [{"value": chrom['chrid'], "label": chrom['chrid']} for chrom in sorted_chromosomes_list]
+    
+    conn.close()
+    return sorted_chromosomes_list
 
 
 """
 Returns the concated dataframe of the chromosome data in the given chromosome name and sequence start and end
 """
-def matched_chromosome_data(data_dir, chromosome_name, chromosomeSequence):
-    start = chromosomeSequence['start']
-    end = chromosomeSequence['end']
 
-    file_pattern = f"{data_dir}/{chromosome_name}.*.*"
-    matching_files = glob.glob(file_pattern)
 
-    selected_files = []
-    for file in matching_files:
-        dir_name = os.path.basename(file)
-        dir_parts = dir_name.split(".")
+# def matched_chromosome_data(data_dir, chromosome_name, chromosomeSequence):
+#     start = chromosomeSequence["start"]
+#     end = chromosomeSequence["end"]
 
-        if len(dir_parts) == 3:
-            try:
-                file_start = int(dir_parts[1])
-                file_end = int(dir_parts[2])
-            except ValueError:
-                print(f"Skipping file due to invalid format: {file}")
-                continue
+#     file_pattern = f"{data_dir}/{chromosome_name}.*.*"
+#     matching_files = glob.glob(file_pattern)
 
-            # Check if the file is within the provided range
-            if file_start >= start and file_end <= end:
-                folder_path = os.path.join(file, 'hic.clean.1', 'hic.clean.csv.gz')
-                selected_files.append(folder_path)
-        else:
-            print(f"Skipping file due to insufficient parts: {file}")
+#     selected_files = []
+#     for file in matching_files:
+#         dir_name = os.path.basename(file)
+#         dir_parts = dir_name.split(".")
 
-    data_frames = [pd.read_csv(file) for file in selected_files]
-    
-    if data_frames:
-        chromosome_data_df = pd.concat(data_frames, ignore_index=True)
-    else:
-        chromosome_data_df = pd.DataFrame()
-    
-    return chromosome_data_df
+#         if len(dir_parts) == 3:
+#             try:
+#                 file_start = int(dir_parts[1])
+#                 file_end = int(dir_parts[2])
+#             except ValueError:
+#                 print(f"Skipping file due to invalid format: {file}")
+#                 continue
+
+#             # Check if the file is within the provided range
+#             if file_start >= start and file_end <= end:
+#                 folder_path = os.path.join(file, "hic.clean.1", "hic.clean.csv.gz")
+#                 selected_files.append(folder_path)
+#         else:
+#             print(f"Skipping file due to insufficient parts: {file}")
+
+#     data_frames = [pd.read_csv(file) for file in selected_files]
+
+#     if data_frames:
+#         chromosome_data_df = pd.concat(data_frames, ignore_index=True)
+#     else:
+#         chromosome_data_df = pd.DataFrame()
+
+#     return chromosome_data_df
